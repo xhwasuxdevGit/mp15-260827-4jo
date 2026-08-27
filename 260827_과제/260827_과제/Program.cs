3eea7103a78733821b40af14d964e4047a97d660
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-08-27 17:30:24 +0900

    과제 프로젝트 시작

---
+++ 260827_과제/260827_과제/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=== 4조 팀원 소개 ===");
+        // 팀원 소개 호출 추가
+        
+        Console.WriteLine("=== 소개 끝 ===");
+    }
+    
+    //팀원소개 메서드 위치
+}
