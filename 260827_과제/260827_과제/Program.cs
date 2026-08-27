d77b39924e5f70744d98e72068677604e61a0037
JuiceJ <joosjae01@gmail.com>
2026-08-27 17:34:17 +0900

    [Feat] 팀원 주성재의 이름을 출력하는 IntroJoosjae01 메서드 선언 및 호출
    Closes #1

--- 260827_과제/260827_과제/Program.cs
+++ 260827_과제/260827_과제/Program.cs
@@ -6,9 +6,15 @@ public class Program
     {
         Console.WriteLine("=== 4조 팀원 소개 ===");
         // 팀원 소개 호출 추가
+
+        IntroJoosjae01();
         
         Console.WriteLine("=== 소개 끝 ===");
     }
     
     //팀원소개 메서드 위치
+    static void IntroJoosjae01()
+    {
+        Console.WriteLine("주성재 / 열심히 하겠습니다. 앞으로 잘 부탁드립니다.");
+    }
 }
